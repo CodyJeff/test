6226e3828f54f33a898dd70bf874e06dd2ad941d
CodyJeff <jeff_potrido@cody.inc>
2025-04-02 11:55:23 +0800

    test

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
 <?php
-header('Location: login.php');
-exit;
+
+define('LARAVEL_START', microtime(true));
+
+// ✅ Point to Laravel's vendor/autoload and bootstrap/app
+require __DIR__ . '/laravel/vendor/autoload.php';
+
+$app = require_once __DIR__ . '/laravel/bootstrap/app.php';
+
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+)->send();
+
+$kernel->terminate($request, $response);
